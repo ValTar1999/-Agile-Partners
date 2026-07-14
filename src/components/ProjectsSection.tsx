5deea7e1245622212ca5355d3fd8a2c61286e513
ValTar1999 <vladtarna25@gmail.com>
2026-07-14 15:40:41 +0300

    -+

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -172,14 +172,14 @@ function ProjectCard({
 }
 
 const MARQUEE_ITEM_CLASS =
-  'flex shrink-0 items-center gap-8 px-4 text-[100px] leading-[100px] font-normal -tracking-[5px] whitespace-nowrap uppercase md:gap-16 md:text-7xl md:-tracking-[4px] lg:gap-36 lg:text-[168px] lg:leading-[168px] lg:-tracking-[8.4px] xl:text-[204px] xl:leading-[168px] xl:-tracking-[10.2px]';
+  'flex shrink-0 items-center gap-8 px-4 text-[100px] leading-[100px] font-normal -tracking-[5px] whitespace-nowrap uppercase md:gap-16 md:text-7xl md:-tracking-[4px] lg:gap-36 lg:text-[168px] lg:leading-[168px] xl:gap-40 lg:-tracking-[8.4px] xl:text-[204px] xl:leading-[168px] xl:-tracking-[10.2px]';
 
 const MARQUEE_PHRASES = 4;
 const MARQUEE_SCROLL_SPEED = 0.45;
 
 function MarqueePhrase() {
   return (
-    <span className="xl:mr-24">
+    <span className="">
       <span className="text-current">WHAT WE DO</span>
       <span className="text-current italic">BEST</span>
     </span>
